56f49bf4f24e2aeb01301df9946f38ab3b361d80
Kenna <kennaalastair@gmail.com>
2019-10-09 16:04:56 -0700

    add and remove features and total

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addItemAC } from '../actions';
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button
+        className="button" onClick={() => props.addItemAC(props.feature)}>
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+//give addItem action to connect function and hooks up to store as a potential action
+export default connect(
+  null, // null is where mapStateToProps function would be but since we don't have any state to display, we put null
+  { addItemAC } // passes action to AdditionalFeature as a prop
+)(AdditionalFeature);
+//AdditionalFeature is already getting mapStateToProps from AdditionalFeatures
